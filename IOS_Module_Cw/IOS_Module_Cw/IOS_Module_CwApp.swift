b80af88eb3308e1d77146fcb547e0d367778ef37
NIBM-LAB04-PC05 <nibm-lab04-pc05@NIBM-LAB04-PC05s-iMac-2.local>
2024-03-16 16:16:09 +0530

    create project

---
+++ IOS_Module_Cw/IOS_Module_Cw/IOS_Module_CwApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Module_CwApp.swift
+//  IOS_Module_Cw
+//
+//  Created by NIBM-LAB04-PC05 on 2024-03-16.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Module_CwApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
